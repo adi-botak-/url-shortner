ce57fed2025d386a5db9b45f53a4415b1df074c6
adi-botak- <adityautamawijaya@gmail.com>
2016-04-14 20:50:28 +0800

    Added 1 new get and 1 post

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'json'
 require 'base64'
+require 'haml'
+require 'uri'
 require_relative 'models/url'
 
 # url shortner web application
@@ -10,7 +12,7 @@ class UrlShortnerAPI < Sinatra::Base
   end
 
   get '/?' do
-    'Hello, World!'
+    haml :index
   end
 
   get '/api/v1/urls/?' do
@@ -19,13 +21,45 @@ class UrlShortnerAPI < Sinatra::Base
     { url_id: id_list }.to_json
   end
 
+  get '/api/v1/urls/:id/*' do
+    content_type 'text/plain'
+
+    begin
+      attribute = params['splat'][0]
+      ShortUrl.find(params[:id]).instance_variable_get("@#{attribute}")
+    rescue => e
+      status 404
+      e.inspect
+    end
+  end
+
   get '/api/v1/urls/:id.json' do
     content_type 'application/json'
+
     begin
       { url: ShortUrl.find(params[:id]) }.to_json
     rescue => e
       status 404
       logger.info "FAILED to GET url: #{e.inspect}"
+    end
+  end
+
+  post '/api/v1/urls/?' do
+    content_type 'application/json'
+
+    begin
+      new_data = JSON.parse(request.body.read)
+      new_url_data = ShortUrl.new(new_data)
+      if new_url_data.save
+        logger.info "NEW URL STORED: #{new_url_data.id}"
+      else
+        halt 400, "Could not store url details: #{new_url_data}"
+      end
+
+      redirect 'api/v1/urls/' + new_url_data.id + '.json'
+    rescue => e
+      status 400
+      logger.info "Failed to create new url: #{e}"
     end
   end
 end
